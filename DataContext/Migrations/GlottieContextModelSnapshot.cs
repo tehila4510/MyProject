f858b6f27c8924b8326285e3f8217adbcee69d1d
Tehila ST <tehila4510@gmail.com>
2026-02-01 00:25:44 +0200

    adding controlers, services and dto

--- DataContext/Migrations/GlottieContextModelSnapshot.cs
+++ DataContext/Migrations/GlottieContextModelSnapshot.cs
@@ -125,6 +125,10 @@ namespace DataContext.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("UserId"));
 
+                    b.Property<string>("AvatarUrl")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
                     b.Property<DateTime>("CreatedAt")
                         .HasColumnType("datetime2");
 
